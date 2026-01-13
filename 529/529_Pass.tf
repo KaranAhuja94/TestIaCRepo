cdc23a6698d04750796f385b796ba75382dcd054
KaranAhuja94 <32140617+KaranAhuja94@users.noreply.github.com>
2026-01-13 12:13:46 +0530

    Delete 529 directory

--- 529/529_Pass.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_launch_configuration" "cid529launchconfigpass" {
-  name              = "terraform-launch-config-pass"
-  image_id          = "ami-06b09bfacae1453cb"
-  instance_type     = "t2.micro"
-  enable_monitoring = true    # Ensure aws_launch_configuration has an attribute enable_monitoring set to true
-}
