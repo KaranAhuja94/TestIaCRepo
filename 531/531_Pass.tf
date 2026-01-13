9db7c2871d36fe0155ad120cc18f8fd3321745eb
KaranAhuja94 <32140617+KaranAhuja94@users.noreply.github.com>
2026-01-13 12:13:55 +0530

    Delete 531 directory

--- 531/531_Pass.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_neptune_cluster" "cid531pass" {
-  cluster_identifier                  = "cid-531-build-time-pass"
-  engine                              = "neptune"
-  # Ensure aws_neptune_cluster resource has attribute storage_encrypted set to true and attribute kms_key_arn has key arn specified
-  storage_encrypted = true
-  kms_key_arn = "arn:aws:kms:us-east-1:860454016470:key/0ee1cd6e-3e7d-4582-b3a5-44b792f6943f"
-}
